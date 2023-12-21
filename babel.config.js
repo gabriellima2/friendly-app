14c6a35919dab28948cd6450cbf76d5ade454829
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-12-21 15:05:54 -0300

    chore: add module alias

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,19 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      require.resolve("expo-router/babel")
+			require.resolve("expo-router/babel"),
+			[require.resolve("babel-plugin-module-resolver"), {
+        root: ["."],
+        extensions: [
+          ".js",
+          ".jsx",
+          ".ts",
+          ".tsx",
+        ],
+        alias: {
+          "@": "./src",
+        }
+			}],
     ]
   };
 };
